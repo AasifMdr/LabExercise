8043af506a041da423dcd7c979fe6c72c936b773
Aasif Manandhar <aasifmanandhar09@gmail.com>
2021-05-31 10:30:08 +0545

    apples

--- LabOne/QnThree.py
+++ LabOne/QnThree.py
@@ -5,9 +5,9 @@ How many apples will each single student get? How many apples will remain in the
 The program reads the numbers N and K. It should print the two answers for the questions above.
 '''
 
-n = int(input('Number of students: '))
-k = int(input('Number of apples: '))
-remaining_apples = k %2
-apples_with_students = int (k/n)
+n=int(input('Number of students: '))
+k=int(input('Number of apples: '))
+remaining_apples=k%2
+apples_with_students=int(k/n)
 print(f'The remaining apples are {remaining_apples}')
 print(f'The apples with students {apples_with_students}')
