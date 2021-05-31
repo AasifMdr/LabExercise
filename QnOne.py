5c90dd83910c964596f7babbc9693b47ee0c06e4
Aasif Manandhar <aasifmanandhar09@gmail.com>
2021-05-31 10:12:18 +0545

    Initial commit

---
+++ QnOne.py
@@ -0,0 +1,10 @@
+'''
+Write a program and takes three numbers and print their sum.
+Every number is given on a separate line.
+'''
+
+num1 = int(input("Enter the first value: "))
+num2 = int(input("Enter the second value: "))
+num3 = int(input("Enter the third value: "))
+sum = num1 + num2 + num3
+print(f"The sum of given three numbers is {sum}")
