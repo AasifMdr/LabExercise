1aa4122800a9f76ca7d5fb15c2df9a882d9b12b2
Aasif Manandhar <aasifmanandhar09@gmail.com>
2021-06-10 14:47:17 +0545

    sts

--- LabOne/Qn5.py
+++ LabOne/Qn5.py
@@ -6,3 +6,19 @@ of desk that can be purchased.
 The program should read 3 int: the numb of sts in each of the 3 classes
 a, b and c resp.
 '''
+
+classa = int(input("Number of students in class A: "))
+classb = int(input("Number of students in class B: "))
+classc = int(input("Number of students in class C: "))
+a = classa // 2
+if classa % 2 == 1:
+    a = a + 1
+b = classb // 2
+if classb % 2 == 1:
+    b = b + 1
+c = classc // 2
+if classc % 2 == 1:
+    c = c + 1
+print(f'The first group has {classa} number of students so they require {a} desk ')
+print(f'The first group has {classb} number of students so they require {b} desk ')
+print(f'The first group has {classc} number of students so they require {c} desk ')
