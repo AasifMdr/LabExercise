609cae7225c2d040b23adb3aaec61d0d78f90ec9
Aasif Manandhar <aasifmanandhar09@gmail.com>
2021-06-10 14:49:08 +0545

    sts

--- LabOne/Qn5.py
+++ LabOne/Qn5.py
@@ -10,15 +10,19 @@ a, b and c resp.
 classa = int(input("Number of students in class A: "))
 classb = int(input("Number of students in class B: "))
 classc = int(input("Number of students in class C: "))
+
 a = classa // 2
 if classa % 2 == 1:
     a = a + 1
+
 b = classb // 2
 if classb % 2 == 1:
     b = b + 1
+
 c = classc // 2
 if classc % 2 == 1:
     c = c + 1
+
 print(f'The first group has {classa} number of students so they require {a} desks ')
 print(f'The first group has {classb} number of students so they require {b} desks ')
 print(f'The first group has {classc} number of students so they require {c} desks ')
