bba59cf0257d681acfe59d232cdac509337d8eac
Aasif Manandhar <aasifmanandhar09@gmail.com>
2021-06-14 09:38:54 +0545

    sum integer

--- LabOne/Qn9.py
+++ LabOne/Qn9.py
@@ -3,6 +3,6 @@ Write a python program to find sum of the first n positive integers.
 sum = (n*(n+1)) / 2
 '''
 
-num = int(input("Enter a number: "))
+num = int(input("Enter a positive integer: "))
 sum = (num * (num + 1)) / 2
 print(f'The sum of the first {num} positive integers is {sum}')
