c624a4a2f6025f36de836659d7d1ee29d735a5ec
Aasif Manandhar <aasifmanandhar09@gmail.com>
2021-06-21 09:20:53 +0545

    fractional part

--- LabTwo/Qn7.py
+++ LabTwo/Qn7.py
@@ -3,6 +3,5 @@ Given a positive real number, print its fractional part.
 '''
 
 num = float(input("Enter a positive real number: "))
-i = int(num)
-f = num - int(num)
-print(f'Its fractional part is {f}')
+fractional_part = num - int(num)
+print(f'Its fractional part is {fractional_part}')
